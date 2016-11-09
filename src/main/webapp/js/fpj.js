8f12be3fadff86fa39f326e7a6c08214cd5a6bff
liuyuanya <524684541@qq.com>
2016-11-09 16:06:52 +0800

    修改发票夹

--- src/main/webapp/js/fpj.js
+++ src/main/webapp/js/fpj.js
@@ -37,6 +37,7 @@ function getToken(code) {
 		method : 'post',
 		success : function(data) {
 			if (data.success) {
+				alert(data.access_token);
 				openid = data.openid;
 				access_token = data.access_token;
 				expires_in = data.expires_in;
@@ -97,7 +98,7 @@ function getUserMsg() {
 		method : 'post',
 		success : function(data) {
 			if (data.success) {
-				
+				alert('成功');
 			} else {
 				alert(data.msg);
 				return;
