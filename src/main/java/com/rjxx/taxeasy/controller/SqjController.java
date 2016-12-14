20104a3f25bf70d002563bb64ea1c26254ca31b6
liuyan@datarj.com <liuyan@datarj.com>
2016-12-14 16:21:07 +0800

    加wdm跳转url

--- src/main/java/com/rjxx/taxeasy/controller/SqjController.java
+++ src/main/java/com/rjxx/taxeasy/controller/SqjController.java
@@ -411,7 +411,13 @@ public class SqjController extends BaseController {
 	public void tqmsstq() throws Exception {
 		response.sendRedirect(request.getContextPath() + "/smtq/sqjss.html?_t=" + System.currentTimeMillis());
 	}
-
+	// 食其家 寿司采用提取码提取方式
+	// 跳转到sqj提取码提取页面
+	@RequestMapping(value = "/wdm")
+	@ResponseBody
+	public void tqmwdmtq() throws Exception {
+		response.sendRedirect(request.getContextPath() + "/smtq/sqjwdm.html?_t=" + System.currentTimeMillis());
+	}
 	// 校验提取码是否正确
 	@RequestMapping(value = "/tqyz")
 	@ResponseBody
