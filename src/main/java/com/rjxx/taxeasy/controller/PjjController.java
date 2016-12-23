03be6a8dcef0952792eca90afb4a44dbbcb8617c
liuyuanya <524684541@qq.com>
2016-12-23 09:18:09 +0800

    修改bug

--- src/main/java/com/rjxx/taxeasy/controller/PjjController.java
+++ src/main/java/com/rjxx/taxeasy/controller/PjjController.java
@@ -1130,7 +1130,8 @@ public class PjjController extends BaseController {
             sb.append("<a href='" + pdfUrl + "'>" + null2Wz(pdfUrl) + "</a><br>");
         }
         sb.append("请及时下载您的发票。");
-        sb.append("<font color='red'>提示:苹果浏览器无法显示发票章,只能下载pdf显示</font><br>");
+        sb.append("<br/><br/>");
+        sb.append("提示:苹果浏览器无法显示发票章,只能下载pdf显示");
         sb.append("<br/><br/>");
         sb.append(gsdm);
         sb.append("<br/>");
