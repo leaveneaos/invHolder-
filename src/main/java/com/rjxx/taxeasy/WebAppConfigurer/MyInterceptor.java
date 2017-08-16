a11fcb2c5af9afb50169625a15c9a8b3f833e242
许黎明 <1084854604@qq.com>
2017-08-16 18:55:20 +0800

    修改支付宝

--- src/main/java/com/rjxx/taxeasy/WebAppConfigurer/MyInterceptor.java
+++ src/main/java/com/rjxx/taxeasy/WebAppConfigurer/MyInterceptor.java
@@ -19,6 +19,16 @@ public class MyInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         HttpSession session=request.getSession();
         String[] url=request.getServletPath().split("/");
         logger.info("-----初始化URL----end-----"+url.length);
@@ -35,15 +45,5 @@ public class MyInterceptor implements HandlerInterceptor {
             }
         }
         logger.info("-----初始化URL----end-----"+request.getServletPath());
-        return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
-    }
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
     }
 }
