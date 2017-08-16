fa7bceb3e394899bdd827766071a0187dadc3cfa
许黎明 <1084854604@qq.com>
2017-08-16 18:47:47 +0800

    修改支付宝拦截

--- src/main/java/com/rjxx/taxeasy/WebAppConfigurer/MyInterceptor.java
+++ src/main/java/com/rjxx/taxeasy/WebAppConfigurer/MyInterceptor.java
@@ -19,11 +19,6 @@ public class MyInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         HttpSession session=request.getSession();
         String[] url=request.getServletPath().split("/");
         logger.info("-----初始化URL----end-----"+url.length);
@@ -31,14 +26,21 @@ public class MyInterceptor implements HandlerInterceptor {
             if (AlipayUtils.isAlipayBrowser(request)) {
                 logger.info("---------判断是否是支付宝浏览器------");
                 if (!AlipayUtils.isAlipayAuthorized(session)) {
-                    logger.info("-----初始化支付宝授权----start-----");
-                    AlipayUtils.initAlipayAuthorization(request, response);
+                    logger.info("-----初始化支付宝授权----strat-----");
+                    AlipayUtils.initAlipayAuthorization(request, response, request.getServletPath());
                     logger.info("-----初始化支付宝授权----end------");
                     logger.info("-----初始化URL----end-----"+request.getServletPath());
+
                 }
             }
         }
         logger.info("-----初始化URL----end-----"+request.getServletPath());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
     }
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
