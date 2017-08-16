eb1212391fee830ab57c3e8606cec3033fef2534
许黎明 <1084854604@qq.com>
2017-08-16 14:35:23 +0800

    修改拦截器

--- src/main/java/com/rjxx/taxeasy/WebAppConfigurer/MyInterceptor.java
+++ src/main/java/com/rjxx/taxeasy/WebAppConfigurer/MyInterceptor.java
@@ -26,7 +26,8 @@ public class MyInterceptor implements HandlerInterceptor {
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         HttpSession session=request.getSession();
         String[] url=request.getServletPath().split("/");
-        if(url.length>1){
+        logger.info("-----初始化URL----end-----"+JSON.toJSONString(url));
+        if(url.length>2){
             if (AlipayUtils.isAlipayBrowser(request)) {
                 logger.info("---------判断是否是支付宝浏览器------");
                 if (!AlipayUtils.isAlipayAuthorized(session)) {
