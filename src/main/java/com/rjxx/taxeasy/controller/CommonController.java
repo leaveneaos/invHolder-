6e5b20e131df98c037bc59b3a38f34a1cea8b528
zhangsongqiang <zhangsongqiang@datarj.com>
2017-08-17 17:04:22 +0800

    修改check

--- src/main/java/com/rjxx/taxeasy/controller/CommonController.java
+++ src/main/java/com/rjxx/taxeasy/controller/CommonController.java
@@ -1,7 +1,11 @@
 package com.rjxx.taxeasy.controller;
 
 import com.rjxx.taxeasy.comm.BaseController;
+import com.rjxx.taxeasy.dao.WxfpxxJpaDao;
+import com.rjxx.taxeasy.domains.WxFpxx;
+import com.rjxx.taxeasy.service.BarcodeService;
 import com.rjxx.utils.weixin.WeixinUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +20,11 @@ import java.util.Map;
 @RequestMapping("/common")
 public class CommonController extends BaseController {
 
+
+    @Autowired
+    private WxfpxxJpaDao wxfpxxJpaDao;
+    @Autowired
+    private BarcodeService barcodeService;
     //判断是否微信浏览器
     @RequestMapping(value = "/isBrowser")
     @ResponseBody
@@ -26,21 +35,32 @@ public class CommonController extends BaseController {
             logger.info("微信浏览器--------------");
             WeixinUtils weixinUtils = new WeixinUtils();
             try {
-                redirectUrl = weixinUtils.getTiaoURL(orderNo,price,orderTime, storeNo);
-                if(null==redirectUrl||redirectUrl.equals("")){
-                   /* resultMap.put("msg","获取微信授权失败!请重试");
-                    resultMap.put("num","2");
-                    return resultMap;*/
+                //查询是否开具
+                WxFpxx wxFpxx = wxfpxxJpaDao.selsetByOrderNo(orderNo);
+                String status = barcodeService.checkStatus(wxFpxx.getTqm(),wxFpxx.getGsdm());
+                if(status!=null&& status.equals("开具中")){
+                    //开具中对应的url
+                    response.sendRedirect(request.getContextPath() + "/QR/zzkj.html?t=" + System.currentTimeMillis());
+                    return null;
+                }else if(status!=null && status.equals("可开具")){
+                    //可开具 跳转微信授权链接
+                    redirectUrl = weixinUtils.getTiaoURL(orderNo,price,orderTime, storeNo);
+                    if(null==redirectUrl||redirectUrl.equals("")){
+                        //获取授权失败
+                        request.getSession().setAttribute("msg", "获取微信授权失败!请重试!");
+                        response.sendRedirect(request.getContextPath() + "/smtq/demo.html?_t=" + System.currentTimeMillis());
+                        return null;
+                    }else {
+                        //成功跳转
+                        response.sendRedirect(redirectUrl);
+                        return null;
+                    }
+                }else {
                     request.getSession().setAttribute("msg", "获取微信授权失败!请重试!");
                     response.sendRedirect(request.getContextPath() + "/smtq/demo.html?_t=" + System.currentTimeMillis());
                     return null;
-                }else {
-                    response.sendRedirect(redirectUrl);
-                    return null;
                 }
 
-               /* resultMap.put("num","0");
-                resultMap.put("redirectUrl",redirectUrl);*/
             } catch (Exception e) {
                 e.printStackTrace();
             }
