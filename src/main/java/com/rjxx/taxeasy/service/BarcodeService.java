e2d206cfb13c29a30991693715d0bb2bf5ef8a50
zhangsongqiang <zhangsongqiang@datarj.com>
2017-08-17 14:35:24 +0800

    微信修改

--- src/main/java/com/rjxx/taxeasy/service/BarcodeService.java
+++ src/main/java/com/rjxx/taxeasy/service/BarcodeService.java
@@ -1,5 +1,10 @@
 package com.rjxx.taxeasy.service;
 
+import com.rjxx.taxeasy.domains.Jymxsq;
+import com.rjxx.taxeasy.domains.Jyxxsq;
+import com.rjxx.taxeasy.domains.Jyzfmx;
+
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,6 +15,16 @@ public interface BarcodeService {
                        String gfsh,String email,String gfyh,
                        String gfyhzh,String gfdz,String gfdh,String tqm,
                        String openid,String sjly);
+
+    /**
+     * 拉取数据
+     * 封装开具
+     * @return
+     */
+    String pullInvioce(Map resultSjMap,String gsdm,String gfmc,
+                       String gfsh,String email,String gfyh,
+                       String gfyhzh,String gfdz,String gfdh,String tqm,
+                       String openid,String sjly ,String access_token);
 
     Map sm(String gsdm, String q);
 
