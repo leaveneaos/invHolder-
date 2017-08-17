173ccd66af2b918a7e0b3e25af4e4ec2a414a0ba
许黎明 <1084854604@qq.com>
2017-08-17 10:25:41 +0800

    修改支付宝

--- src/main/java/com/rjxx/taxeasy/WebAppConfigurer/MyInterceptor.java
+++ src/main/java/com/rjxx/taxeasy/WebAppConfigurer/MyInterceptor.java
@@ -27,7 +27,8 @@ public class MyInterceptor implements HandlerInterceptor {
                 logger.info("---------判断是否是支付宝浏览器------");
                 if (!AlipayUtils.isAlipayAuthorized(session)) {
                     logger.info("-----初始化支付宝授权----strat-----");
-                    AlipayUtils.initAlipayAuthorization(request, response, request.getServletPath());
+                    AlipayUtils.initAlipayAuthorization(request, response, "sendhtml");
+
                     logger.info("-----初始化支付宝授权----end------");
                     logger.info("-----初始化URL----end-----"+request.getServletPath());
                     return true;
