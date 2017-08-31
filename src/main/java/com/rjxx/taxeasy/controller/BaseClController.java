c5714c57f53b1606e04f0b8e153b8d0af8749b22
zhangsongqiang <zhangsongqiang@datarj.com>
2017-08-31 13:23:11 +0800

    update

--- src/main/java/com/rjxx/taxeasy/controller/BaseClController.java
+++ src/main/java/com/rjxx/taxeasy/controller/BaseClController.java
@@ -279,6 +279,9 @@ public class BaseClController extends BaseController {
                     String je = list.get(0).getJshj().toString();
                     SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                     String orderTime = sdf.format(list.get(0).getLrsj());
+                    logger.info("跳转发票详情页面---"+orderNo+"+"+je+"+"+orderTime);
+                    logger.info("跳转连接"+request.getContextPath() + "/Family/fpxq.html?_t="
+                            + System.currentTimeMillis()+ "=" + orderNo + "=" + je + "orderTime=" + orderTime);
                     response.sendRedirect(request.getContextPath() + "/Family/fpxq.html?_t="
                             + System.currentTimeMillis()+ "=" + orderNo + "=" + je + "orderTime=" + orderTime);
                     return;
