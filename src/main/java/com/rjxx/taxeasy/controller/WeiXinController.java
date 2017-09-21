44cec77b61495b0af2ca46c2958c3f310eecb7b7
zhangsongqiang <zhangsongqiang@datarj.com>
2017-09-21 15:49:36 +0800

    no message

--- src/main/java/com/rjxx/taxeasy/controller/WeiXinController.java
+++ src/main/java/com/rjxx/taxeasy/controller/WeiXinController.java
@@ -211,8 +211,6 @@ public class WeiXinController extends BaseController {
                                     logger.info("商品明细为空");
                                     return "";
                                 }
-                                logger.info("开票流水为---"+JSON.toJSONString(kpls));
-                                logger.info("开票商品明细为---"+JSON.toJSONString(kpspmxList));
                                 logger.info("PDFurl---"+pdf_file_url);
                                 //插入卡包
                                 String result = weixinUtils.fpInsertCardBox(SuccOrderId, pdf_file_url, kpspmxList, kpls);
