0ebffa1791d220c074295ede991ace68cd553ae4
wangyahui <24340912@qq.com>
2017-12-11 09:29:24 +0800

    注释掉企查查controller，添加到svc中

--- src/main/java/com/rjxx/taxeasy/controller/QCCController.java
+++ src/main/java/com/rjxx/taxeasy/controller/QCCController.java
@@ -1,33 +1,33 @@
-package com.rjxx.taxeasy.controller;
-
-import com.rjxx.taxeasy.comm.BaseController;
-import com.rjxx.utils.yjapi.QCCUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * Created by wangyahui on 2017/11/27 0027.
- * 企查查API
- */
-@RestController
-@RequestMapping("/companyInfo")
-public class QCCController extends BaseController{
-
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @Autowired
-    private QCCUtils qccUtils;
-
-    @RequestMapping(value = "/getNames", method = RequestMethod.POST)
-    public String getNames(@RequestParam("name")String name) {
-        logger.info("输入的参数为："+name);
-        String result =qccUtils.getQccSearch(name);
-        logger.info("输出为："+result);
-        return result;
-    }
-}
+//package com.rjxx.taxeasy.controller;
+//
+//import com.rjxx.taxeasy.comm.BaseController;
+//import com.rjxx.utils.yjapi.QCCUtils;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * Created by wangyahui on 2017/11/27 0027.
+// * 企查查API
+// */
+//@RestController
+//@RequestMapping("/companyInfo")
+//public class QCCController extends BaseController{
+//
+//    private Logger logger = LoggerFactory.getLogger(this.getClass());
+//
+//    @Autowired
+//    private QCCUtils qccUtils;
+//
+//    @RequestMapping(value = "/getNames", method = RequestMethod.POST)
+//    public String getNames(@RequestParam("name")String name) {
+//        logger.info("输入的参数为："+name);
+//        String result =qccUtils.getQccSearch(name);
+//        logger.info("输出为："+result);
+//        return result;
+//    }
+//}
